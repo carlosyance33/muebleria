6d9321ab6809daeaee120d64878140fcd90f3061
Fleon2023 <fernando.leon.valencia@gmail.com>
2024-03-15 17:08:00 -0400

    css en 3d y javascript

---
+++ scrip.js
@@ -0,0 +1,82 @@
+const miConstante = "hola chamo qlq"
+console.log(miConstante)
+
+
+/* tipo de datos
+string o cadena: una string o cadena de texto se define como un conjunto de caracteres y para definirlos haremos uso de comillas dobles, simples o backtick  */
+
+/*tipo de dato nomber es un tipo de dato numerico nos permite guardar solamente numeros, sea numeros enteros o decimales */ 
+
+/*tipo de dato boolean este es un tipo de dato primitivo que solamente tiene dos valores verdadero y falso */
+
+/*el cero funciona como si fuera una falso y el 1 como si fuera un verdadero */
+
+const lista = [
+    "cebolla",
+    "mantequilla",
+    "aceie",
+    "cebollin",
+    "leche"
+]
+
+console.log (lista)
+
+
+/* un array es una lista de datos ordenados en memoria, uno detras de otro a los cuales se les asigna un indice, cuyo primer indice es 0 , el ultimo indice de todo array es el -1 */
+
+/* en un objeto los datos se guardan con una llave que puedo definir */
+
+const usuario = {
+    name: "Carlos",
+    age: 23,
+    password: "1234",
+    job: "diseñador"
+
+
+}
+
+/* un objeto es un tipo de dato completo, que me permite almacenar datos por medio de clave y por medio de valor */
+
+/*datos negativos
+
+undefiled es un tipo de dato indefinido que espera obtener un valor por lo general este dato los obtenemos cuando algo sale mal
+
+null es un tipo de dato vacio y esta vacio intencionalmente */
+
+/*if (true) {
+    console.log ("se cumplio")
+} */
+
+let variable
+if (variable) {
+    console.log ("se cumplio")
+} else {
+    console.log ("no se cumplio")
+}
+
+/**
+ * 
+ * +   suma
+ * -    resta
+ * *    multiplica
+ * /    divide
+ * %    resto
+ * 
+ * 
+ * ==   esto es igual a
+ * ===  esto es exactamente igual a
+ * !=   esto es diferente
+ * !==  esto es exactamente diferente
+ * 
+ * &&   and
+ * 
+ * ||   or
+ * 
+ * 
+ * >    mayor que 
+ * <    menor que
+ * 
+ * >=   mayor o igual que
+ * <=   Menor o igual que
+ * 
+ */
